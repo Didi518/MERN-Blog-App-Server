5fbc950bb153f8a595e7154f2a55b11010d43aa8
Didi518 <94155950+Didi518@users.noreply.github.com>
2024-01-04 04:47:50 +0100

    :card_file_box: création de la fonction pour récup les commentaires

--- controllers/commentControllers.js
+++ controllers/commentControllers.js
@@ -1,5 +1,5 @@
-import Comment from '../models/Comment';
-import Post from '../models/Post';
+import Comment from "../models/Comment";
+import Post from "../models/Post";
 
 const createComment = async (req, res, next) => {
   try {
@@ -8,7 +8,7 @@ const createComment = async (req, res, next) => {
     const post = await Post.findOne({ slug: slug });
 
     if (!post) {
-      const error = new Error('Post introuvable');
+      const error = new Error("Post introuvable");
       return next(error);
     }
 
@@ -34,7 +34,7 @@ const updateComment = async (req, res, next) => {
     const comment = await Comment.findById(req.params.commentId);
 
     if (!comment) {
-      const error = new Error('Commentaire introuvable');
+      const error = new Error("Commentaire introuvable");
       return next(error);
     }
 
@@ -53,13 +53,73 @@ const deleteComment = async (req, res, next) => {
     await Comment.deleteMany({ parent: comment._id });
 
     if (!comment) {
-      const error = new Error('Commentaire introuvable');
+      const error = new Error("Commentaire introuvable");
       return next(error);
     }
-    return res.json({ message: 'Commentaire supprimé' });
+    return res.json({ message: "Commentaire supprimé" });
   } catch (error) {
     next(error);
   }
 };
 
-export { createComment, deleteComment, updateComment };
+const getAllComments = async (req, res, next) => {
+  try {
+    const filter = req.query.recherche;
+    let where = {};
+    if (filter) {
+      where.desc = { $regex: filter, $options: "i" };
+    }
+    let query = Comment.find(where);
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * pageSize;
+    const total = await Comment.find(where).countDocuments();
+    const pages = Math.ceil(total / pageSize);
+
+    res.header({
+      "x-filter": filter,
+      "x-totalcount": JSON.stringify(total),
+      "x-currentpage": JSON.stringify(page),
+      "x-pagesize": JSON.stringify(pageSize),
+      "x-totalpagecount": JSON.stringify(pages),
+    });
+
+    if (page > pages) {
+      return res.json([]);
+    }
+
+    const result = await query
+      .skip(skip)
+      .limit(pageSize)
+      .populate([
+        {
+          path: "user",
+          select: ["avatar", "name", "verified"],
+        },
+        {
+          path: "parent",
+          populate: [
+            {
+              path: "user",
+              select: ["avatar", "name"],
+            },
+          ],
+        },
+        {
+          path: "replyOnUser",
+          select: ["avatar", "name"],
+        },
+        {
+          path: "post",
+          select: ["slug", "title"],
+        },
+      ])
+      .sort({ updatedAt: "desc" });
+
+    return res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { createComment, deleteComment, getAllComments, updateComment };
